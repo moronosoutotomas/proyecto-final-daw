d58a28ce6eb8eadc6a6a4b343ddc0f8e35580d0b
Tomás Fedora <moronosoutotomas.sangrina@gmail.com>
2025-10-21 21:52:30 +0200

    Retocando un poco la vista de usuarios, creo que va a quedar estancada un tiempo porque no me convence manejar usuarios y hacer todo el CRUD

--- aplicacion/resources/views/admin/users/index.blade.php
+++ aplicacion/resources/views/admin/users/index.blade.php
@@ -2,7 +2,7 @@
 
     <div class="mb-4 flex justify-between items-center">
         <flux:breadcrumbs>
-            <flux:breadcrumbs.item :href="route('dashboard')">Inicio</flux:breadcrumbs.item>
+            <flux:breadcrumbs.item :href="route('home')">Inicio</flux:breadcrumbs.item>
             <flux:breadcrumbs.item>Usuarios</flux:breadcrumbs.item>
         </flux:breadcrumbs>
 
@@ -12,15 +12,15 @@
     </div>
 
     <div class="relative overflow-x-auto mb-4 rounded-lg">
-        @can('filter')
+        @can('admin.users.manage')
             <input wire:model="search"
-                   class="p-3 mb-4 bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                   class="p-3 mb-4 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                    placeholder="Introduce un nome ou un email para filtrar...">
         @endcan
 
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead
-                class="text-center text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 rounded-lg">
+                class="text-center text-xs text-gray-700 uppercase dark:bg-gray-700 dark:text-gray-400 rounded-lg">
             <tr>
                 <th scope="col" class="px-6 py-3">
                     ID
@@ -38,7 +38,7 @@
             </thead>
             <tbody class="rounded-lg">
             @foreach($users as $user)
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <tr class="border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         {{ $user->id }}
                     </th>
@@ -52,12 +52,12 @@
                         <div class="flex space-x-2">
                             <a href="{{ route('admin.users.edit', $user) }}" class="btn btn-blue text-xs">Editar</a>
 
-                            <form class="delete-form" action="{{--{{ route('admin.users.destroy', $user) }}--}}"
+                            <form class="delete-form" action="{{ route('admin.users.destroy', $user) }}"
                                   method="post">
                                 @csrf
                                 @method('delete')
 
-                                <button class="btn btn-red text-xs">Eliminar</button>
+                                <button type="submit" class="btn btn-red text-xs">Eliminar</button>
                             </form>
                         </div>
                     </td>
@@ -66,10 +66,6 @@
             </tbody>
         </table>
     </div>
-
-    {{--    <div class="mt-4">--}}
-    {{--                {{ $users->links() }}--}}
-    {{--    </div>--}}
 
     @push('js')
         <script>
