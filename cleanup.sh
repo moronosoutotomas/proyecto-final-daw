d6153e585c7af913983add2aa91bf626fd83b987
Tomás Fedora <moronosoutotomas.sangrina@gmail.com>
2025-10-24 18:54:53 +0200

    Update cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,18 +1,22 @@
 #!/binbash
 
 # Parámo-los contenedores
-docker stop <containers>
+# docker stop <containers>
+sudo docker stop aplicacion_tomasmorono-nginx aplicacion_tomasmorono-php aplicacion-php-fpm-1 aplicacion_tomasmorono-pgsql aplicacion_tomasmorono-workspace
 
 # Eliminámolos
-docker rm <containers>
+# docker rm <containers>
+docker container prune -af
 
 # Eliminámo-las imaxes
-docker rmi <images>
+# docker rmi <images>
+docker image prune -af
 
 # Eliminám-lo volumen
-docker volume rm <volume>
+# docker volume rm <volume>
+docker volume rm aplicacion_tomasmorono-data-production aplicacion_tomasmorono-public-assets aplicacion_tomasmorono-storage-production aplicacion_tomasmorono-data-development
+docker volume prune -af
 
 ### COIDADO, ELIMINA EN MASA ###
 # Executámo-la limpeza xeral do proxecto
-docker system prune
-
+docker system prune -af
