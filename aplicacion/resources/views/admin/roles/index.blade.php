20cb073bce08635a8cd6f3c0aaa9de100aad7fe2
Tomás Moroño <tomas.morono@siweb.es>
2025-10-29 17:24:41 +0100

    Fondo roles actualizado en harmonia con el resto de la app

--- aplicacion/resources/views/admin/roles/index.blade.php
+++ aplicacion/resources/views/admin/roles/index.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.app>
     @section('title', 'Roles y Permisos')
 
-    <div class="min-h-screen bg-gray-50 dark:bg-gray-900">
+    <div class="min-h-screen bg-gradient-to-bl from-blue-50 to-amber-100 dark:bg-gradient-to-bl dark:from-amber-950 dark:to-blue-950 dark:bg-gray-900">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
             <!-- Header -->
             <div class="mb-8">
